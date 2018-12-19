d59b476fe30dfe6fb89581a2b8720bef6d6b07b1
WilliamZhu <allwefantasy@gmail.com>
2018-12-19 14:42:13 +0800

    Included script ends with ";" in comments will fail

--- streamingpro-mlsql/src/main/java/streaming/dsl/ScriptSQLExec.scala
+++ streamingpro-mlsql/src/main/java/streaming/dsl/ScriptSQLExec.scala
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package streaming.dsl
 
 import java.util.concurrent.ConcurrentHashMap
@@ -88,7 +70,7 @@ object ScriptSQLExec extends Logging with WowLog {
           stop = true
         }
         includes.foreach { f =>
-          wow = wow.replace(f._1, f._2.substring(0, f._2.lastIndexOf(";")))
+          wow = wow.replace(f._1, f._2)
         }
         max_preprocess -= 1
       }
